25f6583195cb188851365894c55a4428475b48d7
sunxiaoshen <sunxiaoshen@threatbook.cn>
2019-08-13 19:31:57 +0800

    support object key multiple level

--- src/server.js
+++ src/server.js
@@ -73,46 +73,64 @@ router.post('/query', async (req, res) => {
 	}
 })
 function repeatData(data, change, repeat, total) {
-	let arr = [];
-	for (var t = 0; t < total / repeat; t++) {
-		change.forEach((item) => {
-			const [key, type] = item.split('#');
-			data[key] = faker.internet[type]();
-		})
-		for (var i = 0; i < repeat; i++) {
-			data = Object.assign({}, data);
-			data.time = ++data.time;
-			data.id = faker.random.uuid();
-			arr.push(data);
-		}
-	}
-	return arr;
+    let arr = [];
+    for (var t = 0; t < total / repeat; t++) {
+        change.forEach((item) => {
+            const [key, type] = item.split('#');
+            setMultipleVal(data, key, type);
+        })
+        for (var i = 0; i < repeat; i++) {
+            data = JSON.parse(JSON.stringify(data));
+            data.time = ++data.time;
+            data.id = faker.random.uuid();
+            arr.push(data);
+        }
+    }
+    return arr;
+}
+function getMultipleVal(data, keys) {
+    let fn = keys.split('.').reduce((total, next) => {
+        return total[next];
+    }, data)
+    if (fn) {
+        return fn();
+    } else {
+        return keys;
+    }
+}
+function setMultipleVal(data, keys, value) {
+    var arr = keys.split('.');
+    var lastKey = arr.pop();
+    var tmpObj = arr.reduce((total, next) => {
+        return total[next];
+    }, data);
+    tmpObj[lastKey] = getMultipleVal(faker, value);
 }
 router.post('/run', async (req, res, next) => {
-	let source = JSON.parse(req.body.source);
-	let change = req.body.change.split('\n');
-	let control = req.body.control.split('\n');
-	let controlObj = {};
-	control.forEach((item) => {
-		const [key, value] = item.split(':');
-		controlObj[key] = isNaN(parseInt(value, 10)) ? value : parseInt(value, 10);
-	})
-	console.log(control);
-	let dataArr = repeatData(source, change, controlObj.repeat || 1, controlObj.total || 1)
-	let body = dataArr.flatMap(source => [
-				{ index: { _index: controlObj.index, _type: "log", _id: source.id  }  },
-				source
-			]);
-	fs.writeFileSync('./data', JSON.stringify(body));
+    let source = JSON.parse(req.body.source);
+    let change = req.body.change.split('\n');
+    let control = req.body.control.split('\n');
+    let controlObj = {};
+    control.forEach((item) => {
+        const [key, value] = item.split(':');
+        controlObj[key] = isNaN(parseInt(value, 10)) ? value : parseInt(value, 10);
+    })
+    console.log(control);
+    let dataArr = repeatData(source, change, controlObj.repeat || 1, controlObj.total || 1)
+    let body = dataArr.flatMap(source => [
+                { index: { _index: controlObj.index, _type: "log", _id: source.id  }  },
+                source
+            ]);
+    fs.writeFileSync('./data', JSON.stringify(body));
 
-	try {
-		const result = await client.bulk({
-			refresh: true,
-			body
-		})
-		res.send(result);
-	} catch(e) {
-		res.send(e);
-	}
+    try {
+        const result = await client.bulk({
+            refresh: true,
+            body
+        })
+        res.send(result);
+    } catch(e) {
+        res.send(e);
+    }
 })
 export default server;
